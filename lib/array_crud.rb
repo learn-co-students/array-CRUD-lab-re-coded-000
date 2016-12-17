e671241370f2cf497735fa35d6b4ea4efc7d7e91
leloz suliman <leloz997@gmail.com>
2016-12-17 17:04:00 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+array=["erbil","duhok","qamishlo","sulaymanya"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array=["erbil","duhok","qamishlo","sulaymanya"]
+array.push(element)#"derek"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+array=["erbil","duhok","qamishlo","sulaymanya"]
+array.unshift(element)#"karkuk"
 end
 
 def remove_element_from_end_of_array(array)
-  
+#array=["erbil","duhok","qamishlo","sulaymanya","karkuk"]
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+#array=["erbil","duhok","qamishlo","sulaymanya"]
+array.shift#("karkuk")
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+#array=["erbil","duhok","qamishlo","sulaymanya"]
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  #array=["erbil","duhok","qamishlo","sulaymanya"]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  #array=["erbil","duhok","qamishlo","sulaymanya"]
+  array.last
 end
