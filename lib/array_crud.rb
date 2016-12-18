72d4e2e90ea8fe1c9cb0e3fdb40fafa8696527f4
nichervan brhik <nichervan.brhik@gmail.com>
2016-12-18 13:09:22 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,34 +8,28 @@ students=["hasan","shero","brzan","malik"]
 end
 
 def add_element_to_end_of_array(array, element)
-students=["hasan","shero","brzan","malik"]
-students << "arrays!"
+array << element
 end
 def add_element_to_start_of_array(array, element)
-students=["hasan","shero","brzan","malik"]
-students.unshift "wow"
+array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
-  students=["hasan","shero","brzan","malik"]
-  students << "arrays!"
-students.pop
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  students=["hasan","shero","brzan","malik"]
-  students.unshift "wow"
-  students.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-"am"
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-"wow"
+array.first
 end
 
 def retrieve_last_element_from_array(array)
-"arrays!"
+array.last
 end
